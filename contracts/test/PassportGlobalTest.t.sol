daa0ef29533166429f167b13e20420e98ec3d0e2
robbie <robbiecsumner@gmail.com>
2023-08-10 11:39:58 +0200

    tests and npm script to deploy

---
+++ contracts/test/PassportGlobalTest.t.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import {Test} from "forge-std/Test.sol";
+import {PassportGlobal} from "../src/PassportNFT.sol";
+
+contract PassportNFTTest is Test {
+  PassportGlobal passportGlobal;
+  address USER = makeAddr("user");
+  address ANOTHER_USER = makeAddr("another user");
+
+  uint256 constant STARTING_AMOUNT = 1 ether;
+
+  function setUp() external {
+    passportGlobal = new PassportGlobal();
+    vm.deal(USER, STARTING_AMOUNT);
+  }
+
+  modifier passportMinted() {
+    vm.prank(USER);
+    passportGlobal.mint();
+    _;
+  }
+
+  function testUserCanMintPassport() external passportMinted {
+    assertEq(passportGlobal.balanceOf(USER), 1);
+    assertEq(passportGlobal.ownerOf(0), USER);
+  }
+
+  function testUserCanOnlyMintOnce() external passportMinted {
+    vm.expectRevert(
+      abi.encodeWithSelector(
+        PassportGlobal.PassportGlobal__AddressCanOnlyMintOnce.selector,
+        USER
+      )
+    );
+    vm.prank(USER);
+    passportGlobal.mint();
+  }
+
+  function testUserCannotTransferPassport() external passportMinted {
+    vm.expectRevert(
+      abi.encodeWithSelector(
+        PassportGlobal.PassportGlobal__NotTransferable.selector
+      )
+    );
+    vm.prank(USER);
+    passportGlobal.transferFrom(USER, ANOTHER_USER, 0);
+  }
+}
