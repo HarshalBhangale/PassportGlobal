3b288eae24c9670afaf63d5dd9bcee33397db2f9
Julio M Cruz <julio.cruz@eb-ms.net>
2023-08-11 01:57:02 -0400

    Update background

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
 
 export function LandingPage() {
 
   return (
     <>
-      <h1>Landing Page</h1>
+      <div className="w-full  bg-indigo-950 flex items-center justify-center"
+        style={{ height: "100vh" }}>
+        <div className="flex">
+          <ConnectButton />
+        </div>
+
+      </div>
     </>
   );
 
