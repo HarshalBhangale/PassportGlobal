0af1f68dd6c9d5538c1a30df21c3675fd388c203
Denis Perov <denis.perov@gmail.com>
2023-08-12 20:44:39 +0300

    Support creating actual stamps on-chain via smart contract

--- src/pages/QRScan.tsx
+++ src/pages/QRScan.tsx
@@ -1,37 +1,50 @@
-import { useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
-import { useAccount } from 'wagmi';
-import { usePassportGlobalGetPassport, usePassportGlobalGetPassportId, usePassportGlobalHasPassport } from '../generated';
+import { useAccount ,useWaitForTransaction} from 'wagmi';
+import {
+  usePassportGlobalGetPassport,
+  usePreparePassportGlobalGrantStamp,
+  usePassportGlobalGrantStamp,
+} from '../generated';
 
 export function QRScanPage() {
   const { searchParams } = new URL(window.location.toString());
-  const target = searchParams.get('target');
-  const country = searchParams.get('country');
+  const target = searchParams.get('target') as '0x';
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
+  const country = searchParams.get('country');
   const account = useAccount();
   const navigate = useNavigate();
   const { data: passport, isLoading: passportLoading } = usePassportGlobalGetPassport({
     args: [account.address!],
   });
-  function createStamp() {
-    // Use `target` (as chain address), `lat`, `lng`, `country`...
-    //TODO: Create attestation via AttestationStation
-    //TODO: Wait for transaction to complete
-    navigate(`/?account=${target}`); //TODO: Support viewing Passports by ID
-  }
+  const { config, isLoading: grantStampPreparing } = usePreparePassportGlobalGrantStamp({
+    args: [
+      target,
+      BigInt(Math.round(Number(lat) * 10**6)),
+      BigInt(Math.round(Number(lng) * 10**6)),
+      country!,
+    ],
+  });
+  const { data, write: grantStamp, isLoading: grantStampDoing } = usePassportGlobalGrantStamp(config);
+  const { isLoading: grantStampWaiting } = useWaitForTransaction({
+    hash: data?.hash,
+    onSuccess: () => navigate(`/account=${target}`),
+  });
   if (!passportLoading) {
     const [name] = passport;
     return (
-        <div className="text-center pt-10">
-          <div className="text-2xl">Do you confirm meeting <span className="text-green-500">{name}</span> in <span className="text-purple-500">{country}</span>?</div>
-          <div className="mt-20">
-            <button className="btn btn-lg bg-yellow-500 text-black w-52" onClick={createStamp}>Yes we've met</button>
-          </div>
-          <div className="mt-10">
-            <button className="btn btn-lg bg-red-500 text-white w-52" onClick={() => navigate('/')}>No (Cancel)</button>
-          </div>
+      <div className="text-center pt-10">
+        <div className="text-2xl">Do you confirm meeting <span className="text-green-500">{name}</span> in <span className="text-purple-500">{country}</span>?</div>
+        {grantStampPreparing && <div className="mt-4">Just a moment…</div>}
+        {grantStampDoing && <div className="mt-4">Granting a stamp…</div>}
+        {grantStampWaiting && <div className="mt-4">Confirming the transaction on chain…</div>}
+        <div className="mt-20">
+          <button className="btn btn-lg bg-yellow-500 text-black w-52" onClick={grantStamp} disabled={grantStampPreparing || grantStampDoing || grantStampWaiting}>Yes we've met</button>
         </div>
+        <div className="mt-10">
+          <button className="btn btn-lg bg-red-500 text-white w-52" onClick={() => navigate('/')} disabled={grantStampPreparing || grantStampDoing || grantStampWaiting}>No (Cancel)</button>
+        </div>
+      </div>
     );
   }
   return <div>Loading...</div>;
